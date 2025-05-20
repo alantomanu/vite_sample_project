afb4e7a9fe777789dcdb2073e535a349dd6a1094
Alanto Manu <alantomanu501@gmail.com>
2025-05-20 08:06:08 +0530

    done some code reated to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from "react"
 
-
-function Blog({title,content}){
-  return(
-    <><h1>{title}</h1>
-    <p>{content}</p></>
-    
-  )
-
-}
 function App() {
-  const blogs=[
-    {title:"blog1",content:"blog1 content1"},
-    {title:"blog2",content:"blog1 content2"},
-    {title:"blog3",content:"blog1 content3"},
-    {title:"blog4",content:"blog1 content4"},
-    {title:"blog5",content:"blog1 content5"},
-  ]
-  
-const [count, StateCount]=useState(0)
+  const data={
+    "kerala":"trivandrum",
+    "karnataka":"bengaluru",
+    "TN":"chenai"
+  }
+const [selectedState, setSelectedState]=useState("")
+const changeState=(e)=>{
+console.log(e.target.value)
+setSelectedState(e.target.value);
+}
   return (
     <>
-    <h1>{count}</h1>
-    <button onClick={()=>StateCount(count + 1)}>+</button>
-    <div className="text-center text-4xl text-red-900">hello</div>
     <div>
-      <h1>Blogs</h1>
-      <hr/>
-      {blogs.map(blog=><Blog title={blog.title} content={blog.content}/>)}
+      <select onChange={changeState}>
+        <option value="kerala">Kerala</option>
+        <option value="karnataka">karnataka</option>
+        <option value="TN">TN</option>
+      </select>
+      <p>Selected State is:{selectedState}</p>
     </div>
     </>
     
